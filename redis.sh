46aaeac3f0de7e36ece0bc9f9e13cfbcc6775a7b
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-01-31 18:00:01 +0530

    Added new code

---
+++ redis.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD  # or $(pwd)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USER_ID -gt 0 ]; then
+    echo -e " $R Please run this script with root user :) $N" | tee -a $LOGS_FILE
+    exit 3;
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# tee command is used to write the output to log file as well as to the console
+VALIDATE(){  
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    else 
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf list installed redis &>>$LOGS_FILE
+if [ $? -ne 0 ]
+    dnf module disable redis -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling Redis Module"
+
+    dnf module enable redis:7 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling Redis 7 Module"
+
+    dnf install redis -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Redis Server"
+else
+    echo -e "Redis already installed... $Y SKIPPING $N"
+fi
+
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
+VALIDATE $? "Update listen address"
+
+sed -i 's/protected-mode yes/protected-mode no/' /etc/redis/redis.conf
+VALIDATE $? "Disabling protected mode"
+
+systemctl enable redis &>>$LOGS_FILE
+VALIDATE $? "Enabling Redis Service"
+
+systemctl start redis &>>$LOGS_FILE
+VALIDATE $? "Starting Redis Service"
