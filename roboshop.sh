6aa2b6c6653ee26efffca7d5d0bef72c2da9ed1f
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-01-31 12:55:55 +0530

    Added new code

--- roboshop.sh
+++ roboshop.sh
@@ -2,9 +2,13 @@
 
 SG_ID="sg-04303efbdaed5c6f9"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z02030392LMN8KQVWR9UZ"
+RECORD_NAME="dawsmani.site"
 
 for instance in $@
 do
+    #Creating the Instance for the given component
+    echo "Creating Instance for $instance"
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
@@ -20,6 +24,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
+        RECORD_NAME="$DOMAIN_NAME"
     else
         IP=$(
             aws ec2 describe-instances \
@@ -27,5 +32,34 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
+
+    echo "Creating Route53 Record for $instance with IP $IP"
+
+    # UPSERT: If record available just update, if not available just create it
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT", 
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+    echo "Record updated for $instance with IP $IP"
+    
 done
