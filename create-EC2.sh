655a73c7bf9e57ca45040c9ae498f868cd0f07fe
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-03 11:55:08 +0530

    Added new code

--- create-EC2.sh
+++ create-EC2.sh
@@ -5,19 +5,21 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z02030392LMN8KQVWR9UZ" #Route53 Hosted Zone ID
 DOMAIN_NAME="dawsmani.site"
 
-for instance in $@
+instance=("mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping"  "rabbitmq" "payment" "frontend")
+
+for name in ${instance[@]}
 do
     #Creating the Instance for the given component
-    echo "Creating Instance for $instance"
+    echo "Creating Instance for $name"
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instance == frontend ]; then
+    if [ $name == frontend ]; then
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
@@ -32,10 +34,10 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        RECORD_NAME="$name.$DOMAIN_NAME"
     fi
 
-    echo "Creating Route53 Record for $instance with IP $IP"
+    echo "Creating Route53 Record for $name with IP $IP"
 
     # UPSERT:: If record available just update, if not available just create it
     aws route53 change-resource-record-sets \
